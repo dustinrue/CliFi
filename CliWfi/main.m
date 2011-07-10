2e062313eea3f48fef3fc6ea0c055b94f9bed702
Dustin Rue <ruedu@dustinrue.com>
2011-07-10 16:39:48 -0500

    Initial Commit

---
+++ CliWfi/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CliWfi
+//
+//  Created by Dustin Rue on 7/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
